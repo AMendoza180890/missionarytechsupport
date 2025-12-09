fc29e531db3c39062ba69e21bd604a3e8c454f4a
AMendoza <allanmaleman@gmail.com>
2025-12-09 10:46:05 -0800

    FIX: Se mejora el contenido de la API

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -3,44 +3,71 @@ const apiUrl = `${domain}/wp-json/wp/v2`;
 
 //TODO: Incluir GraphQL para optimizar las consultas.
 
-export const getLastestPost = async ({ perPage = 10 }: { perPage?: number }) => {
-  const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&_embed`);
+export const getLastestPost = async ({
+  perPage = 10,
+}: {
+  perPage?: number;
+}) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&_embed`);
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch posts: ${response.statusText}`);
-  } else {
-    const result = await response.json();
-    //   console.log(posts);
-    if (!result.length) {
-        throw new Error('No posts found');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch posts: ${response.statusText}`);
+    } else {
+      const result = await response.json();
+      //   console.log(posts);
+      if (!result.length) {
+        throw new Error("No posts found");
+      }
+
+      const posts = result.map(
+        (post: {
+          _embedded?: any;
+          title?: any;
+          content?: any;
+          excerpt?: any;
+          date?: string;
+          slug?: string;
+        }) => {
+          const {
+            title: { rendered: title },
+            content: { rendered: content },
+            excerpt: { rendered: excerpt },
+            date,
+            slug,
+          } = post;
+
+          const featuredImage =
+            post._embedded?.["wp:featuredmedia"]?.[0]?.source_url;
+          return { title, content, excerpt, date, slug, featuredImage };
+        }
+      );
+
+      return posts;
     }
-
-    const posts = result.map((post: { _embedded?: any; title?: any; content?: any; excerpt?: any; date?: string; slug?: string; }) => {
-        const {
-            title: { rendered: title }, 
-            content: { rendered: content }, 
-            excerpt: { rendered: excerpt }, 
-            date, 
-            slug 
-        }= post;
-
-        const featuredImage = post._embedded?.['wp:featuredmedia']?.[0]?.source_url;
-        return { title, content, excerpt, date, slug, featuredImage };
-    })
-    
-    return posts;
+  } catch (error) {
+    console.error("Error fetching latest posts:", error);
+    throw error;
   }
 };
 
-export const getPostInfo = async ( slug: string ) => {
-  const response = await fetch(`${apiUrl}/posts?slug=${slug}`);
-
-  if (!response.ok) {
-    throw new Error(`Failed to fetch posts: ${response.statusText}`);
-  } else {
-    const [result] = await response.json();
-    const {title: { rendered: title }, content: { rendered: content}} = result;
-
-    return { title, content  };
+export const getPostInfo = async (slug: string) => {
+  try{
+    const response = await fetch(`${apiUrl}/posts?slug=${slug}`);
+  
+    if (!response.ok) {
+      throw new Error(`Failed to fetch posts: ${response.statusText}`);
+    } else {
+      const [result] = await response.json();
+      const {
+        title: { rendered: title },
+        content: { rendered: content },
+      } = result;
+  
+      return { title, content };
+    }
+  }catch (error) {
+    console.error("Error fetching post info:", error);
+    throw error;
   }
 };
